ed6d0ffca0e97f6092094441679a6309b0979622
motomotoyoshi <motomoto.git@gmail.com>
2023-04-05 01:19:42 +0900

    init

---
+++ hello-world/.cljs_node_repl/hello_world/core.js
@@ -0,0 +1,6 @@
+// Compiled by ClojureScript 1.11.54 {:target :nodejs, :nodejs-rt true, :optimizations :none}
+goog.provide('hello_world.core');
+goog.require('cljs.core');
+cljs.core.println.call(null,"Hello world!");
+
+//# sourceMappingURL=core.js.map
---
+++ hello-world/out/hello_world/core.js
@@ -0,0 +1,8 @@
+// Compiled by ClojureScript 1.11.54 {:optimizations :none}
+goog.provide('hello_world.core');
+goog.require('cljs.core');
+goog.require('react_dom');
+hello_world.core.global$module$react_dom = goog.global["ReactDOM"];
+ReactDOM.render(React.createElement("h2",null,"Hello, React!"),document.getElementById("app"));
+
+//# sourceMappingURL=core.js.map
